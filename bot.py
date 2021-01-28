84da6f3970f36634b360183d854f2f14de080fb1
Alexsander <alexsander@ubuntu-vm-study.europe-west4-a.c.ambient-antenna-277110.internal>
2021-01-28 17:26:03 +0000

    A new function sorteio added

--- bot.py
+++ bot.py
@@ -30,9 +30,10 @@ START_TEXT = 'Bot para ajudar a sortear uma lista de amigos ocultos'
 HELP_TEXT = """
 Lista de comandos:
 /help - Apresenta esta lista
-/amigos - adiciona amigos ao sorteio
+/add - adiciona amigos ao sorteio
 /apagar - apaga toda a lista de amigos
 /sorteio - recebe nomes e entrega o resultado do sorteio
+/lista - apsenta todos os amigos ja colocados na lista
 """
 
 amigos = []
@@ -43,50 +44,39 @@ def start(update, context):
 def help(update, context):
   """Send a message when the command /help is issued."""
   update.message.reply_text(HELP_TEXT)
-def echo(update, context):
-  """Echo the user message."""
-  update.message.reply_text(update.message.text)
-def amigos(update, context):
-    update.message.reply_text("Envie o nome de cada amigo ou digite 'parar' para encerrar o programa :")
-    get_name = []
-    get_name.append(update.message.text)
-    if "parar" not in get_name:
-        for i in get_name:
-            friends.append(i)
-def secret_santa(update, context):
-    update.message.reply_text("O resultado do sorteio é: ")
-    rand_friends = random.shuffle(amigos)
-    rand_friends.append(rand_friends[0])
-    gift_dict = {}
-    for i in range(len(rand_friends) - 1):
-        gift_dict[rand_friends[i]] = rand_friends[i+1]
-    for k in gift_dict:
-        update.message.reply_text(f'{k} presentei-a {gift_dict[k]}')
-def put(update, context):
-    """Usage: /put value"""
-    # Generate ID and seperate value from command
-    #key = str(uuid4())
+def add(update, context):
+    """Usage: /add friends name"""
     # We don't use context.args here, because the value may contain whitespaces
     value = update.message.text.partition(' ')[2]
-    lst = list(value.split(" "))
+    lst = (value.split(" "))
     key = lst[0]
     emailF = lst[1]
-    # Store value
+    # Store value in dict user_data
     context.user_data[key] = emailF
     # Send the key to the user
-    update.message.reply_text(key + " foi adicionado a lista de amigos.")
+    update.message.reply_text(key + " agora esta na lista de amigos para o sorteio.")
 
-def get(update, context):
-    """Usage: /get uuid"""
+def lista(update, context):
+    """Usage: /lista uuid"""
     # Seperate ID from command
-    key = context.args[0]
-
+    #key = context.args[0]
     # Load value and send it to the user
-    value = context.user_data.get(key, 'Not found')
+    #value = context.user_data.get(key, 'Not found')
     update.message.reply_text(context.user_data)
-  # Create the Updater and pass it your bot's token.
-  # Make sure to set use_context=True to use the new context based callbacks
-  # Post version 12 this will no longer be necessary
+def sorteio(update, context):
+    """Usage: /sorteio uuid"""
+    lst = []
+    for k,v in context.user_data.items():
+        lst.append(k)
+    random.shuffle(lst)
+    names.append(lst[0])
+    gift_dict = {}
+    for i in range(len(friends) - 1):
+            gift_dict[names[i]] = names[i+1]
+    return gift_dict
+# Create the Updater and pass it your bot's token.
+# Make sure to set use_context=True to use the new context based callbacks
+# Post version 12 this will no longer be necessary
 updater = Updater("1679932632:AAH01E9PBfHBKHYkkm2sLnr4gyYd23ET5Uo")
 
 def stop_and_restart():
@@ -114,12 +104,10 @@ def main():
   # On different commands - answer in Telegram
   dp.add_handler(CommandHandler("start", start))
   dp.add_handler(CommandHandler("help", help))
-  dp.add_handler(CommandHandler("sorteio", secret_santa))
-  dp.add_handler(CommandHandler("put", put))
-  dp.add_handler(CommandHandler("get", get))
+  dp.add_handler(CommandHandler("add", add))
+  dp.add_handler(CommandHandler("lista", lista))
+  dp.add_handler(CommandHandler("sorteio", sorteio))
   dp.add_handler(CommandHandler('r', restart, filters=Filters.user(username='@lexsys')))
-  # In noncommand i.e message - echo the message on Telegram
-  dp.add_handler(MessageHandler(Filters.text, echo))
   # log all errors
   dp.add_error_handler(error)
 
